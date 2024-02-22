2adb1e784ec6d181aec1249f37ac6fa9007436cd
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2024-02-22 23:42:13 +0600

    new

---
+++ project1/src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css"
+
+const App = () => {
+
+
+    return (
+    <div>
+        <nav>
+            <div className = "logo">
+            <img src="/images/brand_logo.png" alt="logo" />
+            </div>
+
+            <ul>
+                <li href = "#"> MENU</li>
+                <li href = "#"> LOCATION</li>
+                <li href = "#"> ABOUT</li>
+                <li href = "#"> CONTACT</li>
+            </ul>
+
+            <button>Login</button>
+        </nav>
+    </div>
+    );
+};
+
+
+export default App;
