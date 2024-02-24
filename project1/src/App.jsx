1cf5461f2fde9c13a5c2d70b47d784e812bf0b36
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2024-02-24 10:11:36 +0600

    after cretaing navigation file

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -1,25 +1,12 @@
 import "./App.css";
+import Navigation from "./components/Navigation";
 
 const App = () => {
 
-
     return (
-    <div>
-        <nav>
-            <div className = "logo">
-            <img src="/images/brand_logo.png" alt="logo" />
-            </div>
-
-            <ul>
-                <li href = "#"> MENU</li>
-                <li href = "#"> LOCATION</li>
-                <li href = "#"> ABOUT</li>
-                <li href = "#"> CONTACT</li>
-            </ul>
-
-            <button>Login</button>
-        </nav>
-    </div>
+        <div>
+            <Navigation/>
+        </div>
     );
 };
 
