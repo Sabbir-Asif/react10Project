c6f678d930424ae6ccebad5e354e1f07472ffaa4
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2024-02-24 11:46:28 +0600

    project1 done

--- project1/src/components/Hero.jsx
+++ project1/src/components/Hero.jsx
@@ -6,21 +6,23 @@ const HeroSection = () => {
                 <p>
                     YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.
                 </p>
+
+                <div className="hero-button">
+                    <button>Shop Now</button>
+                    <button className="secondary-button">Category</button>
+                </div>
+
+                <div className="shopping">
+                    <p>Also Available On</p>
+
+                    <div className="brand-icons">
+                        <img src="/images/flipkart.png" alt="amazon-logo" />
+                        <img src="/images/amazon.png" alt="flipkart-logo" />
+                    </div>
+                </div>
+
             </div>
 
-            <div className="hero-button">
-                <button>Shop Now</button>
-                <button className="secondary-button">Category</button>
-            </div>
-
-            <div className="shopping">
-                <p>Also Available On</p>
-            </div>
-
-            <div className="brand-icons">
-                <img src="/images/flipkart.png" alt="amazon-logo" />
-                <img src="/images/amazon.png" alt="flipkart-logo" />
-            </div>
             <div className="hero-image">
                 <img src="/images/shoe_image.png" alt="shoe-image" />
             </div>
