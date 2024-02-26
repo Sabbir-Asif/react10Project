7f25c4d13b2ca3b6ef4e52868f56acd37a2eb561
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2024-02-26 15:46:51 +0600

    after navigation

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -1,13 +1,14 @@
+import React, { useState } from 'react';
 import './App.css'
-
+import Navigation from './components/Navigation/Navigation.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      
+      <Navigation />
     </div>
   );
 };
 
-export default App
+export default App;
---
+++ project2/src/components/Navigation/Navigation.jsx
@@ -0,0 +1,18 @@
+const Navigation = () => {
+  return (
+    <nav className="container">
+        <div className="logo">
+            <img src="/images/logo.png" alt = "do some coding logo" />
+        </div>
+
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+
+    </nav>
+  );
+};
+
+export default Navigation;
